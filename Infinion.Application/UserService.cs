91459b19de858f1019f8f182f517a320208463ab
Ibrahim Suleiman <ebeeraheem@gmail.com>
2024-06-18 15:45:25 +0100

    Add register model

--- Infinion.Application/UserService.cs
+++ Infinion.Application/UserService.cs
@@ -1,8 +1,20 @@
-﻿namespace Infinion.Application;
+﻿using Infinion.Domain;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+
+namespace Infinion.Application;
 public class UserService
 {
-    public UserService()
+    private readonly UserManager<ApplicationUser> _userManager;
+    private readonly SignInManager<ApplicationUser> _signInManager;
+    private readonly IConfiguration _configuration;
+
+    public UserService(UserManager<ApplicationUser> userManager,
+        SignInManager<ApplicationUser> signInManager, 
+        IConfiguration configuration)
     {
-        
+        _userManager = userManager;
+        _signInManager = signInManager;
+        _configuration = configuration;
     }
 }
---
+++ Infinion.Domain/RegisterModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Infinion.Domain;
+public class RegisterModel
+{
+    [Required]
+    public string Email { get; set; } = string.Empty;
+
+    [Required]
+    public string Password { get; set; } = string.Empty;
+
+    [Required]
+    public string FirstName { get; set; } = string.Empty;
+
+    [Required]
+    public string LastName { get; set; } = string.Empty;
+}
