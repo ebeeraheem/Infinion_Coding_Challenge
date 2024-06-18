3672a958a67fe68d31605b1e6d6bed54783457b6
Ibrahim Suleiman <ebeeraheem@gmail.com>
2024-06-18 19:53:38 +0100

    Map RegisterModel to ApplicationUser

--- Infinion.Application/UserService.cs
+++ Infinion.Application/UserService.cs
@@ -28,7 +28,9 @@ public class UserService : IUserService
         var user = new ApplicationUser
         {
             UserName = model.Email,
-            Email = model.Email
+            Email = model.Email,
+            FirstName = model.FirstName,
+            LastName = model.LastName
         };
 
         // Identity hashes passwords by default
