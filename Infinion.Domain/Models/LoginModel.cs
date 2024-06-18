5bc091481b54e8a2c94b2dadbd274e2212dce1b6
Ibrahim Suleiman <ebeeraheem@gmail.com>
2024-06-19 00:09:02 +0100

    Add annotations for data validation for the LoginModel

--- Infinion.Domain/Models/LoginModel.cs
+++ Infinion.Domain/Models/LoginModel.cs
@@ -3,9 +3,13 @@
 namespace Infinion.Domain.Models;
 public class LoginModel
 {
-    [Required]
+    [Required(ErrorMessage = "Email is required.")]
+    [EmailAddress(ErrorMessage = "Invalid email format.")]
     public string Email { get; set; } = string.Empty;
 
-    [Required]
+    [Required(ErrorMessage = "Password is required.")]
+    [MinLength(8, ErrorMessage = "Password must be at least 8 characters long.")]
+    [DataType(DataType.Password)]
+    [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$", ErrorMessage = "Password must be at least 8 characters long and contain at least one lowercase letter, one uppercase letter, and one digit.")]
     public string Password { get; set; } = string.Empty;
 }
