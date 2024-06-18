a9cbbbbf3b37d0b63942a6131e772f51610914dc
Ibrahim Suleiman <ebeeraheem@gmail.com>
2024-06-18 15:20:26 +0100

    Add domain layer

---
+++ Infinion.Domain/ApplicationUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Infinion.Domain;
+public class ApplicationUser : IdentityUser
+{
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+}
