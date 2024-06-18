4cf4ea9f3d7462dcc9c461db74fbf7735854eb4f
Ibrahim Suleiman <ebeeraheem@gmail.com>
2024-06-18 17:02:07 +0100

    Add LoginUserAsync method to UserService.cs

--- Infinion.Application/UserService.cs
+++ Infinion.Application/UserService.cs
@@ -1,27 +1,35 @@
 ﻿using Infinion.Domain;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using System.Security.Policy;
+using System.Text;
 
 namespace Infinion.Application;
 public class UserService
 {
     private readonly UserManager<ApplicationUser> _userManager;
     private readonly SignInManager<ApplicationUser> _signInManager;
-    private readonly IConfiguration _configuration;
+    private readonly IConfiguration _config;
 
     public UserService(UserManager<ApplicationUser> userManager,
         SignInManager<ApplicationUser> signInManager, 
-        IConfiguration configuration)
+        IConfiguration config)
     {
         _userManager = userManager;
         _signInManager = signInManager;
-        _configuration = configuration;
+        _config = config;
     }
 
     public async Task<IdentityResult> RegisterUserAsync(RegisterModel model)
     {
-        var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+        var user = new ApplicationUser 
+            { 
+                UserName = model.Email, 
+                Email = model.Email 
+            };
 
         // Identity hashes passwords by default
         var result = await _userManager.CreateAsync(user, model.Password);
@@ -29,9 +37,43 @@ public class UserService
         if (result.Succeeded)
         {
             var token = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+            // Generate confirmation link
             // Send confirmation email
         }
 
         return result;
+    }
+
+    public async Task<string?> LoginUserAsync(LoginModel model)
+    {
+        var result = await _signInManager.PasswordSignInAsync(
+            model.Email, model.Password, false, false);
+
+        if (!result.Succeeded)
+            return null;
+
+        var user = await _userManager.FindByEmailAsync(model.Email);
+        var authClaims = new List<Claim>
+        {
+            new (ClaimTypes.Name, user.UserName),
+            new (JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+        };
+
+        var authSigningKey = new SymmetricSecurityKey(
+            Encoding.UTF8.GetBytes(_config.GetValue<string>("Jwt:Key")));
+
+        var signingCredentials = new SigningCredentials(
+            authSigningKey, SecurityAlgorithms.HmacSha256);
+
+        // Generate token
+        var token = new JwtSecurityToken(
+        issuer: _config.GetValue<string>("Jwt:Issuer"),
+        audience: _config.GetValue<string>("Jwt:Audience"),
+        claims: authClaims,
+        notBefore: DateTime.UtcNow,
+        expires: DateTime.UtcNow.AddMinutes(3),
+        signingCredentials: signingCredentials);
+
+        return new JwtSecurityTokenHandler().WriteToken(token);
     }
 }
