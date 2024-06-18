592146b61cbd0b5384633c75470c813561aff25d
Ibrahim Suleiman <ebeeraheem@gmail.com>
2024-06-18 20:58:30 +0100

    Create EmailService

---
+++ Infinion.Application/Services/EmailService.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Infinion.Application.Services;
+public class EmailService
+{
+    private readonly IConfiguration _config;
+
+    public EmailService(IConfiguration configuration)
+    {
+        _config = configuration;
+    }
+
+    public async Task SendEmailAsync(string toEmail, string subject, string message)
+    {
+
+    }
+}
