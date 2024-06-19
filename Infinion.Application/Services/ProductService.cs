15c526df50595043878451368f8c628b96313462
Ibrahim Suleiman <ebeeraheem@gmail.com>
2024-06-19 14:09:08 +0100

    Add ProductService.cs

---
+++ Infinion.Application/Services/ProductService.cs
@@ -0,0 +1,12 @@
+﻿using Infinion.Infrastructure.Data;
+
+namespace Infinion.Application.Services;
+public class ProductService
+{
+    private readonly ApplicationDbContext _context;
+
+    public ProductService(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+}
