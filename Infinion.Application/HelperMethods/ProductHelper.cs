51fb47ab30d83ce28214426c049217e7643e3b19
Ibrahim Suleiman <ebeeraheem@gmail.com>
2024-06-19 17:36:13 +0100

    Add FilterProducts method in ProductHelper.cs

---
+++ Infinion.Application/HelperMethods/ProductHelper.cs
@@ -0,0 +1,24 @@
+﻿using Infinion.Domain.Entities;
+
+namespace Infinion.Application.HelperMethods;
+public static class ProductHelper
+{
+    public static IEnumerable<Product> FilterProducts(
+        this IEnumerable<Product> products,
+            string? category = null,
+            decimal? minPrice = null,
+            decimal? maxPrice = null,
+            bool? inStock = null,
+            string? name = null,
+            DateTime? startDate = null,
+            DateTime? endDate = null,
+            string? sortBy = null,
+            string? sortOrder = "asc",
+            int page = 1,
+            int pageSize = 10)
+    {
+
+
+        return products;
+    }
+}
