6d3119eba7ac8b8c566e0692d6dd938ae8d9c925
Ibrahim Suleiman <ebeeraheem@gmail.com>
2024-06-19 17:46:00 +0100

    Implement the FilterProducts method

--- Infinion.Application/HelperMethods/ProductHelper.cs
+++ Infinion.Application/HelperMethods/ProductHelper.cs
@@ -1,4 +1,6 @@
 ﻿using Infinion.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
 
 namespace Infinion.Application.HelperMethods;
 public static class ProductHelper
@@ -17,7 +19,55 @@ public static class ProductHelper
             int page = 1,
             int pageSize = 10)
     {
+        if (!string.IsNullOrWhiteSpace(category))
+        {
+            products = products.Where(p => p.Category == category);
+        }
 
+        if (minPrice.HasValue)
+        {
+            products = products.Where(p => p.Price >= minPrice.Value);
+        }
+
+        if (maxPrice.HasValue)
+        {
+            products = products.Where(p => p.Price <= maxPrice.Value);
+        }
+
+        if (inStock.HasValue)
+        {
+            products = products.Where(
+                p => inStock.Value ? p.Stock > 0 : p.Stock == 0);
+        }
+
+        if (!string.IsNullOrWhiteSpace(name))
+        {
+            products = products.Where(p => p.Name.Contains(name));
+        }
+
+        if (startDate.HasValue)
+        {
+            products = products.Where(p => p.CreatedAt >= startDate.Value || p.LastUpdatedAt >= startDate.Value);
+        }
+
+        if (endDate.HasValue)
+        {
+            products = products.Where(p => p.CreatedAt <= endDate.Value || p.LastUpdatedAt <= endDate.Value);
+        }
+
+        if (!string.IsNullOrWhiteSpace(sortBy))
+        {
+            if (sortOrder.ToLower() == "desc")
+            {
+                products = products.OrderByDescending(
+                    p => EF.Property<object>(p, sortBy));
+            }
+            else
+            {
+                products = products.OrderBy(
+                    p => EF.Property<object>(p, sortBy));
+            }
+        }
 
         return products;
     }
