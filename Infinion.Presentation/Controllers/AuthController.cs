4ee68f7aca89ebfc6f803f0a44889a23d133f8be
Ibrahim Suleiman <ebeeraheem@gmail.com>
2024-06-19 11:31:01 +0100

    Add XML comments to AuthController.cs

--- Infinion.Presentation/Controllers/AuthController.cs
+++ Infinion.Presentation/Controllers/AuthController.cs
@@ -16,6 +16,18 @@ public class AuthController : ControllerBase
         _userService = userService;
     }
 
+    /// <summary>
+    /// Registers a new user in the system.
+    /// </summary>
+    /// <param name="model">The user registration information including email, password, first name, and last name.</param>
+    /// <returns>
+    /// An <see cref="IActionResult"/> that returns:
+    /// <list type="bullet">
+    /// <item><description>200 OK if registration is successful.</description></item>
+    /// <item><description>400 Bad Request if the registration information is invalid.</description></item>
+    /// <item><description>500 Internal Server Error if there is an issue with the server.</description></item>
+    /// </list>
+    /// </returns>
     [HttpPost("register")]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
@@ -44,6 +56,18 @@ public class AuthController : ControllerBase
         }
     }
 
+    /// <summary>
+    /// Logs in an existing user and generates a JWT token.
+    /// </summary>
+    /// <param name="model">The login information including email and password.</param>
+    /// <returns>
+    /// An <see cref="IActionResult"/> that returns:
+    /// <list type="bullet">
+    /// <item><description>200 OK with a JWT token if login is successful.</description></item>
+    /// <item><description>400 Bad Request if the login information is invalid.</description></item>
+    /// <item><description>401 Unauthorized if the user credentials are incorrect.</description></item>
+    /// </list>
+    /// </returns>
     [HttpPost("login")]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
@@ -65,6 +89,19 @@ public class AuthController : ControllerBase
         return Ok(new { Token = token });
     }
 
+    /// <summary>
+    /// Confirms the email of a newly registered user.
+    /// </summary>
+    /// <param name="token">The email confirmation token sent to the user.</param>
+    /// <param name="email">The email address of the user to be validated.</param>
+    /// <returns>
+    /// An <see cref="IActionResult"/> that returns:
+    /// <list type="bullet">
+    /// <item><description>200 OK if email confirmation is successful.</description></item>
+    /// <item><description>400 Bad Request if the token or email is invalid.</description></item>
+    /// <item><description>500 Internal Server Error if there is an issue with the server.</description></item>
+    /// </list>
+    /// </returns>
     [HttpGet("confirmemail")]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
