0619a0f11e0fbd168ac9fc3822a18f08a43981eb
Ibrahim Suleiman <ebeeraheem@gmail.com>
2024-06-19 12:13:15 +0100

    Add LoginResult to improve error handling robustness

---
+++ Infinion.Domain/Results/LoginResult.cs
@@ -0,0 +1,7 @@
+﻿namespace Infinion.Domain.Results;
+public class LoginResult
+{
+    public bool Succeeded { get; set; }
+    public string? ErrorMessage { get; set; }
+    public string? Token { get; set; }
+}
